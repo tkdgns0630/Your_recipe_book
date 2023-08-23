153130dd165dd6e4f5a3c562a326e17da5424c2e
Tristenh <tristenhobba1@gmail.com>
2023-08-23 17:51:54 +1000

    imported handlebars into express, created views/layouts/main.handlebars and public/css/styles.css, render handlebars in controllers index

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const Project = require('../models/Project');
 
 router.use('/api', apiRoutes);
+// route to get all dishes
+router.get('/', async (req, res) => {
+  // We find all dishes in the db and set the data equal to dishData
+  const projectData = await Project.findAll().catch((err) => {
+    res.json(err);
+  });
+  const projects = projectData.map((project) => project.get({ plain: true }));
+  res.render('all', { projects });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
-
+const exphbs = require('express-handlebars');
+const path = require('path');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+// create instaance of handlebars engine
+const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
+// configure handlebars as the default template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+// serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 const sess = {
   secret: 'Super secret secret',
@@ -14,8 +21,8 @@ const sess = {
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
@@ -24,6 +31,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+// handling unmatched routes and serving  404
+app.get('*', (req, res) => {
+  res.status(404).send('dish not found');
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
