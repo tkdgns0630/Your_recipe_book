f5c77f1d7e91cff46bb8e5572318300620da04eb
Your Name <sakshiarora245gmail.comqqaaa git config --global user.email sakshiarora245gmail.com>
2023-08-26 17:29:15 +1000

    dynamic category

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,54 +2,20 @@ const router = require('express').Router();
 const Category = require('../models/Category');
 const Recipe = require('../models/Recipe');
 
-const categories= [
-  {
-    id: 1,
-    category_name: 'asian',
-   
-  },
-  {
-    id: 2,
-    category_name: 'Italian',
-   
-  },
-  {
-    id: 3,
-    category_name: 'Indian',
-   
-  }
-]
 
-const recipes= [
-  {
-    id: 1,
-    name: 'asian',
-   
-  },
-  {
-    id: 2,
-    name: 'Italian',
-   
-  },
-  {
-    id: 3,
-    name: 'Indian',
-   
-  }
-]
 // route to get all categories and recipies
 router.get('/', async (req, res) => {
-  // const categoryData = await Category.findAll();
-  // const recipeData = await Recipe.findAll();
+  const categoryData = await Category.findAll();
+  const recipeData = await Recipe.findAll();
 
-  // const categories = categoryData.map((category) =>
-  //   category.get({ plain: true })
-  // );
-  // const recipes = recipeData.map((recipie) => recipie.get({ plain: true }));
+  const categories = categoryData.map((category) =>
+    category.get({ plain: true })
+  );
+  const recipes = recipeData.map((recipie) => recipie.get({ plain: true }));
 
-  // console.log('Categories:', categories);
-  // console.log('Recipes:', recipes);
- res.render('all', { recipes });
+  console.log('Categories:', categories);
+  console.log('Recipes:', recipes);
+ res.render('all', {recipes,categories});
 
  
 });
