1b4c2467d09f08f77961b9219cf0b447ed25b8d6
tkdgns0630 <tkdgns0630@gmail.com>
2023-08-27 20:33:59 +1000

    connection update

--- config/connection.js
+++ config/connection.js
@@ -11,9 +11,9 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
+      host:'localhost',
       dialect: 'mysql',
-      port: 3306
+      port: process.env.PORT || 3306
     }
   );
 }
