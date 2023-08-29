20513ae37cc59fed3032428f92418d553de3c371
Tristenh <tristenhobba1@gmail.com>
2023-08-29 22:21:54 +1000

    added route to get category by id including recipe model data, renderrecipe name in handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -14,6 +14,21 @@ router.get('/', async (req, res) => {
   res.render('all', { recipes, categories, logged_in: loggedIn });
 });
 
+//  get Recipe by Category id
+router.get('/:id', async (req, res) => {
+  try {
+    const recipePktData = await Category.findByPk(req.params.id, {
+      include: [{ model: Recipe }],
+    });
+    const recipePK = recipePktData.get({ plain: true });
+    // res.json(recipePK);
+    // console.log(recipePK);
+    res.render('all', { recipePK });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
   try {
@@ -23,7 +38,7 @@ router.get('/profile', withAuth, async (req, res) => {
       // include: [{ model: UserFavourites }],
     });
     const user = userData.get({ plain: true });
-   
+
     res.render('profile', {
       ...user,
       logged_in: true,
