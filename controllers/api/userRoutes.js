8c147a151b2f77d4d8e5f0c6b26b6871d2c05e9f
Your Name <sakshiarora245gmail.comqqaaa git config --global user.email sakshiarora245gmail.com>
2023-08-31 15:54:25 +1000

    changes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
+router.get('/login',async (req, res) => {
+  try {
+    const userData = await User.findAll();
+    console.log(userData);
+    res.status(200).json(userData);
+
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
