7c5544d270fe91b194c16729cf9e955b2fa49dee
Your Name <sakshiarora245gmail.comqqaaa git config --global user.email sakshiarora245gmail.com>
2023-08-31 17:34:08 +1000

    merge conflicts

--- public/js/addRecipe.js
+++ public/js/addRecipe.js
@@ -4,9 +4,9 @@ const newFormHandler = async (event) => {
   const name = document.querySelector('#recipe-name').value.trim();
   const ingredients = document.querySelector('#recipe-ingredients').value.trim();
   const method = document.querySelector('#recipe-desc').value.trim();
-  const catId = document.querySelector('#recipe-cat').selectedIndex;
-  const prepTime = document.querySelector('input[name="prep-time"]:checked').value;
-  const hasNuts = document.querySelector('#nuts:checked')!==null;
+  const cat_id = document.querySelector('#recipe-cat').selectedIndex;
+  const prep_time = document.querySelector('input[name="prep-time"]:checked').value;
+  const has_nuts = document.querySelector('#nuts:checked')!==null;
   const vegan = document.querySelector('#vegan:checked')!==null;
   const photo = document.querySelector('#input-files').files[0];
 
@@ -14,13 +14,13 @@ const newFormHandler = async (event) => {
   formData.append("name",name);
   formData.append("ingredients",ingredients);
   formData.append("method",method);
-  formData.append("prepTime",prepTime);
-  formData.append("catId",catId);
+  formData.append("prep_time",prep_time);
+  formData.append("cat_id",cat_id);
   formData.append("vegan",vegan);
-  formData.append("hasNuts",hasNuts);
+  formData.append("has_nuts",has_nuts);
   formData.append("file",photo)
 
-  if (name && ingredients && method && prepTime && catId ) {
+  if (name && ingredients && method && prep_time && cat_id ) {
     const response = await fetch(`/api/add-recipe`, {
       method: 'POST',
       body: formData,
