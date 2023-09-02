ffc5ea94a0c80d3e4dfd62b69a91a8d4fb679106
Your Name <sakshiarora245gmail.comqqaaa git config --global user.email sakshiarora245gmail.com>
2023-09-03 03:36:57 +1000

    like fix on refresh

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -1,6 +1,7 @@
 const likeBtn = document.querySelector('#like-btn');
 const whiteHeart = '\u2661';
 const blackHeart = '\u2665';
+//change like button text
 var toggleButtonText = function (recipeId, action) {
   if (action === whiteHeart) {
     likeBtn.textContent = blackHeart;
@@ -15,6 +16,9 @@ var newEventHandler = async function (event) {
   if (event.target.hasAttribute('data-id')) {
     const recipeId = event.target.getAttribute('data-id');
     const action = event.target.textContent.trim();
+    if (action === whiteHeart) {
+      localStorage.setItem('btnTxt', action);
+    }
     const response = await fetch(`/api/recipes/${recipeId}`, {
       method: 'PUT',
       body: JSON.stringify({ recipeId, action }),
@@ -28,7 +32,6 @@ var newEventHandler = async function (event) {
     }
   }
 };
-
 
 const commentFormHandler = async (event) => {
   event.preventDefault();
@@ -63,3 +66,12 @@ document
   .querySelector('.new-comment-form')
   .addEventListener('submit', commentFormHandler);
 
+window.onload = () => {
+  const a = localStorage.getItem('btnTxt');
+  if (a === whiteHeart) {
+    likeBtn.textContent = blackHeart;
+    localStorage.setItem('btnTxt', blackHeart);
+  } else {
+    likeBtn.textContent = whiteHeart;
+  }
+};
